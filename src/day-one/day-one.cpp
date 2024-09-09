242ee964b222c7ef7911d8e17bf786c86aa663f6
Sky Kosiner <sky@skykosiner.com>
2024-09-09 06:29:50 +0100

    day two part one

--- src/day-one/day-one.cpp
+++ src/day-one/day-one.cpp
@@ -5,35 +5,33 @@
 #include <unordered_map>
 #include <vector>
 
-using namespace std;
-
 int combine_items(char x, char y) {
-  string string_deez_nuts = string(1, x) + string(1, y);
+  std::string string_deez_nuts = std::string(1, x) + std::string(1, y);
   return stoi(string_deez_nuts);
 }
 
-bool replace(string &str, const string &from, const string &to) {
+bool replace(std::string &str, const std::string &from, const std::string &to) {
   size_t start_pos = str.find(from);
-  if (start_pos == string::npos)
+  if (start_pos == std::string::npos)
     return false;
   str.replace(start_pos, from.length(), to);
   return true;
 }
 
 int part_one() {
-  ifstream infile("./day-one.input");
+  std::ifstream infile("./day-one.input");
 
   if (!infile) {
-    cout << "it's joever\n";
+    std::cout << "it's joever\n";
     return -1;
   }
 
-  string line;
-  vector<int> numbers;
+  std::string line;
+  std::vector<int> numbers;
   int sum = 0;
 
   while (getline(infile, line)) {
-    vector<char> nums;
+    std::vector<char> nums;
     for (char &c : line) {
       if (isdigit(c)) {
         nums.push_back(c);
@@ -51,16 +49,16 @@ int part_one() {
 }
 
 int part_two() {
-  ifstream infile("./day-one-test-two.input");
+  std::ifstream infile("./day-one-test-two.input");
 
   if (!infile) {
-    cout << "it's joever\n";
+    std::cout << "it's joever\n";
     return -1;
   }
 
-  string line;
-  vector<int> numbers;
-  unordered_map<string, string> map_deez_nuts{
+  std::string line;
+  std::vector<int> numbers;
+  std::unordered_map<std::string, std::string> map_deez_nuts{
       {"one", "one1one"},       {"two", "two2two"},
       {"three", "three3three"}, {"four", "four4four"},
       {"five", "five5five"},    {"six", "six6six"},
@@ -70,7 +68,7 @@ int part_two() {
   int sum = 0;
 
   while (getline(infile, line)) {
-    vector<char> nums;
+    std::vector<char> nums;
     for (const auto &item : map_deez_nuts) {
       replace(line, item.first, item.second);
     }
@@ -92,8 +90,8 @@ int part_two() {
 }
 
 int main() {
-  cout << part_one() << "\n";
-  cout << part_two() << "\n";
+  std::cout << part_one() << "\n";
+  std::cout << part_two() << "\n";
 
   return 0;
 }
--- src/day-two/day-two.cpp
+++ src/day-two/day-two.cpp
@@ -1,19 +1,117 @@
 #include <cctype>
+#include <cstddef>
 #include <fstream>
 #include <iostream>
+#include <map>
+#include <sstream>
 #include <string>
+#include <vector>
 
-using namespace std;
+struct Game {
+  int id;
+};
+
+bool replace(std::string &str, const std::string &from, const std::string &to) {
+  size_t start_pos = str.find(from);
+  if (start_pos == std::string::npos)
+    return false;
+  str.replace(start_pos, from.length(), to);
+  return true;
+}
+
+std::vector<std::string> split_string_for_sets(const std::string &input) {
+  std::vector<std::string> result;
+
+  std::size_t colonPos = input.find(':');
+  if (colonPos != std::string::npos) {
+    std::string valuesPart = input.substr(colonPos + 1);
+
+    std::stringstream ss(valuesPart);
+    std::string segment;
+
+    while (std::getline(ss, segment, ';')) {
+      result.push_back(segment);
+    }
+  }
+
+  return result;
+}
+
+std::vector<std::string> split_string_for_set(const std::string &input) {
+  std::vector<std::string> result;
+  std::stringstream ss(input);
+  std::string segment;
+
+  while (std::getline(ss, segment, ',')) {
+    result.push_back(segment);
+  }
+
+  return result;
+}
+
+std::string extract_color(const std::string &input) {
+  std::istringstream ss(input);
+  std::string number, color;
+
+  // Extract the first part (the number) into 'number'
+  ss >> number;
+
+  // Extract the second part (the color) into 'color'
+  ss >> color;
+
+  return color;
+}
+
+bool parse_items(std::vector<std::string> sets) {
+  std::map<std::string, int> limits = {
+      {"red", 12},
+      {"green", 13},
+      {"blue", 14},
+  };
+
+  for (std::string set : sets) {
+    std::vector<std::string> single_item = split_string_for_set(set);
+
+    for (std::string single : single_item) {
+      int value = std::stoi(single.substr(0, *" "));
+      std::string color = extract_color(single);
+      auto max = limits.find(color);
+
+      if (value > max->second) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+}
 
 int part_one() {
-  ifstream infile("./day-one.input");
+  std::ifstream infile("./day-two.input");
+  std::vector<Game> games;
+  size_t sum = 0;
 
   if (!infile) {
-    cout << "it's joever\n";
+    std::cout << "it's joever\n";
     return -1;
   }
 
-  string line;
+  std::string line;
+
+  while (getline(infile, line)) {
+    std::string id = line.substr(0, line.find(":"));
+    replace(id, "Game ", "");
+
+    std::vector<std::string> sets = split_string_for_sets(line);
+    if (parse_items(sets))
+      games.push_back(Game{std::stoi(id)});
+  }
+
+  for (Game game : games) {
+    sum += game.id;
+  }
+
+  return sum;
 }
 
 int main() {
